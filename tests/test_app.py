472bae1d9ff4379a72efa7ae9ae7b798c80a5d5d
Caio Morelli <caiomorelli.it@gmail.com>
2026-02-08 16:19:46 -0300

    Testando endpoint com return em HTML

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,18 @@ from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
+client = TestClient(app)
+
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_exercicio_aula02():
+    response = client.get('/exercicio02')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
