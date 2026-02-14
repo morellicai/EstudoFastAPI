7c468ea7a521d8f2a155cc823c09ca88ee60eec5
Caio Morelli <caiomorelli.it@gmail.com>
2026-02-14 20:30:18 -0300

    Criando o primeiro CRUD no curso

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema, UserList
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,17 +14,14 @@ def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-@app.get(
-    '/exercicio02', status_code=HTTPStatus.OK, response_class=HTMLResponse
-)
-def exercicio_aula02():
-    return """
-    <html>
-      <head>
-        <title>Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1> Olá Mundo </h1>
-      </body>
-    </html>
-    """
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,26 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-client = TestClient(app)
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_exercicio_aula02():
-    response = client.get('/exercicio02')
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
 
-    assert response.status_code == HTTPStatus.OK
-    assert '<h1> Olá Mundo </h1>' in response.text
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
