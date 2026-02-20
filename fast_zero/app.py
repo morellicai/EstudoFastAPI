79f243715d4e69e611809d8f56c102009612705f
Caio Morelli <caiomorelli.it@gmail.com>
2026-02-20 13:41:46 -0300

    Implementando rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema, UserList
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -22,6 +22,35 @@ def create_user(user: UserSchema):
 
     return user_with_id
 
+
 @app.get('/users/', response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+print(database)
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
